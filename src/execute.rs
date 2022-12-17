0b22659b2e3197614936b304a70e25ca84b72670
Boris Belmar <borisbelmarm@gmail.com>
2022-12-17 20:20:55 -0300

    Improve code

--- src/execute.rs
+++ src/execute.rs
@@ -3,12 +3,15 @@ use std::process::{Command, Stdio};
 pub fn get_event_handler <'a>(command: &'a str) -> impl Fn() + 'a {
   return move || {
     if command != "" {
-      Command::new("sh")
+      println!("Command executed!");
+      let output = Command::new("sh")
         .arg("-c")
         .arg(command)
         .stdout(Stdio::piped())
         .output()
         .expect("failed to execute process");
+
+      println!("{}", String::from_utf8_lossy(&output.stdout));
     }
   };
 }
